860f1198c11ef211363246fce244903e4d638cb9
KatePril <ket14pril14@gmail.com>
2023-06-27 14:24:37 +0300

    Add directory paths

---
+++ src/app/utils/Constants.java
@@ -0,0 +1,6 @@
+package app.utils;
+
+public final class Constants {
+    public final static String BASE_PATH = "D:/Java/Hillel/Java Pro/ht/File-Work-NIO-task";
+    public final static String BASE_PATH_IN = "files/";
+}
